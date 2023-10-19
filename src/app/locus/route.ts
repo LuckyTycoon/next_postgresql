303a23c18a9361ccbb5e4075d406efad8761b6db
dp2genius <dp2genius@gmail.com>
2023-10-19 09:57:43 -0700

    feat: Add other filters

--- src/app/locus/route.ts
+++ src/app/locus/route.ts
@@ -1,12 +1,15 @@
 import { getErrorResponse } from "@/lib/helpers";
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
+import { ADMIN, LIMITED, NORMAL } from "../api/data";
 
 export async function GET(req: NextRequest) {
-    // const userId = req.headers.get("X-USER-ID");
-    // if (!userId) {
-    //     return getErrorResponse(401, "You are not logged in, please provide token to gain access");
-    // }
+    const userId = req.headers.get("X-USER-ID");
+    let role = LIMITED;
+
+    if (userId) {
+        console.log(userId);
+    }
 
     // Filters
     const url = new URL(req.nextUrl);
@@ -15,22 +18,51 @@ export async function GET(req: NextRequest) {
     const regionId = url.searchParams.get('regionId'); // `regionId`, enum, (rld table)
     const membershipStatus = url.searchParams.get('membershipStatus'); // `membershipStatus`, varchar,single value (rld table)
 
+    // Pagination
+    const pageSize = parseInt(url.searchParams.get('pageSize') || '100');
+    const page = parseInt(url.searchParams.get('page') || '1');
+
     // Side loading
     const sideloading = url.searchParams.get('sideloading');
 
-    console.log('--------------------------------------');
-    console.log(Id, assemblyId, regionId, membershipStatus);
-
     const query = {} as any;
 
+    // Helper function
     const itemQuery = (cond: Boolean, obj: any) => cond ? obj : null;
 
+    // Filters
     query.where = {
         ...itemQuery(Id !== '', { id: BigInt(Id) }),
-        ...itemQuery(assemblyId !== null, { assembly_id: assemblyId })
+        ...itemQuery(assemblyId !== null, { assembly_id: assemblyId }),
+
+        // For side loading
+        rnc_locus_members: {
+            some: {
+                ...itemQuery(regionId !== null, { region_id: parseInt(regionId || '-1') }),
+                ...itemQuery(membershipStatus !== null, { membership_status: membershipStatus })
+            }
+        }
     };
 
-    query.take = 10;
+    // Pagination
+    query.take = pageSize;
+    query.skip = (page - 1) * pageSize;
+
+    // Look up
+    // const subQuery = {} as any;
+    // subQuery.where = {
+    //     ...itemQuery(regionId !== null, { region_id: parseInt(regionId || '-1') }),
+    //     ...itemQuery(membershipStatus !== null, { membership_status: membershipStatus })
+    // };
+
+    // Side Loading
+    if ((sideloading === '1') && (role !== NORMAL)) {
+        query.include = {
+            rnc_locus_members: true // subQuery
+        };
+    }
+
+    // Find Items
 
     try {
         const result = await prisma.rnc_locus.findMany(query);
--- src/lib/api-requests.ts
+++ src/lib/api-requests.ts
@@ -70,3 +70,7 @@ export async function apiGetAuthUser(token?: string | null): Promise<FilteredUse
     // console.log('apiGetAuthUser', response);
     return handleResponse<UserResponse>(response).then(({ data }) => data.user);
 }
+
+export async function apiGetTable() {
+    
+}
